374d0d22c00684856015235319129edfa5f3e896
TajiriS <37666928+TajiriS@users.noreply.github.com>
2019-01-25 00:08:59 -0500

    Add files via upload

---
+++ TarifaCajaCompensacion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Precios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Programa para calcular su tarifa de caja de compensación familiar");
+            Console.WriteLine("Por favor ingrese su salario");
+            double salario = double.Parse(Console.ReadLine());
+            bool A = false;
+            bool B = false;
+            bool C = false;
+            double tarifa = 30000;
+            double porcentaje = (salario * 4) / 100;
+
+            if (salario < 1656232)
+            {
+                A = true;
+            }
+
+            if (salario >= 1656232 && salario < 3312464)
+            {
+                B = true;
+                tarifa = tarifa * 1.4;
+            }
+
+            if (salario >= 3312464)
+            {
+                C = true;
+                tarifa = tarifa * 1.6;
+            }
+
+            double resultado = tarifa + porcentaje;
+
+            if (A == true)
+            {
+                Console.WriteLine("Usted está en la categoría A, por lo que su tarifa es: " + tarifa + ", Y debe pagar: " + resultado);
+            }
+
+            if (B == true)
+            { 
+            Console.WriteLine("Usted está en la categoría B, por lo que su tarifa es: " + tarifa + ", Y debe pagar: " + resultado);
+            }
+
+            if (C == true)
+            {
+
+                Console.WriteLine("Usted está en la categoría C, por lo que su tarifa es: " + tarifa + ", Y debe pagar: " + resultado);
+            }
+        }
+    }
+}
---
+++ TarifaEPS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TarifaMedica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular su tarifa de EPS");
+            Console.WriteLine("Por favor ingrese su salario");
+            double salario = double.Parse(Console.ReadLine());
+            bool A = false;
+            bool B = false;
+            bool C = false;
+            double tarifa = 4000;
+
+            if(salario < 1656232)
+            {
+                A = true;
+            }
+
+            if(salario >= 1656232 && salario < 3312464)
+            {
+                B = true;
+                tarifa = tarifa * 1.4;
+
+            }
+
+            if(salario >= 3312464)
+            {
+                C = true;
+                tarifa = tarifa * 1.6;
+            }
+
+            if(A == true)
+            {
+                Console.WriteLine("Su cuota moderadora es de 3200 y su tarifa es de " + tarifa);
+            }
+
+            if (B == true)
+            {
+                Console.WriteLine("Su cuota moderadora es de 12700 y su tarifa es de " + tarifa);
+            }
+
+            if (C == true)
+            {
+                Console.WriteLine("Su cuota moderadora es de 33500 y su tarifa es de " + tarifa );
+            }
+        }
+    }
+}
